863b1bf41be832322008714b0da202c1a3658568
shinnkura <n.shinzo1010@icloud.com>
2023-09-14 01:12:16 +0900

    update : Headerの背景カラーを追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,22 @@ const Header = () => {
   return (
     <header>
         <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
+            <div className='
+                absolute
+                top-0
+                left-0
+                w-full
+                h-96
+                bg-gradient-to-br
+                from-pink-400
+                to-[#0055D1]
+                rounded-md
+                fileter
+                blur-3xl
+                opacity-50
+                z-[-50]
+            ' />
+
             <Image
                 src="https://links.papareact.com/c2cdd5"
                 alt="Trello Logo"
@@ -30,7 +46,7 @@ const Header = () => {
             </div>
         </div>
 
-        <div className='flex items-center justify-center px-5 md:py-5'>
+        <div className='flex items-center justify-center px-5 py-2 md:py-5'>
             <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]'>
                 <UserCircleIcon className="inline-block h-6 w-6 text-[#0055D1] mr-1" />
                 summarising shinnkura tasks for the day ...
