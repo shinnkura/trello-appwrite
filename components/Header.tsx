e2a7a970c889d754f6deef51ee964358aaac73c5
shinnkura <n.shinzo1010@icloud.com>
2023-09-14 00:20:48 +0900

    Add : headerの完成

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,33 @@
+'use client';
+
 import Image from 'next/image'
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import Avatar from 'react-avatar';
 
 
 const Header = () => {
   return (
     <header>
-        <Image
-            // src="https://www.trello.com/images/header-logo-blue.svg"
-            src="https://links.papareact.com/c2cdd5"
-            alt="Trello Logo"
-            width={300}
-            height={100}
-            className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
-        />
-        <div>
-            {/* Search Box */}
-            <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
-                <MagnifyingGlassIcon className="h-6 w-6 text-gray-500" />
-                <input type="text" placeholder='Search' className='flex-1 outline-none p-2' />
-                <button type="submit" hidden>Search</button>
-            </form>
+        <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
+            <Image
+                src="https://links.papareact.com/c2cdd5"
+                alt="Trello Logo"
+                width={300}
+                height={100}
+                className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+            />
 
-            {/* Avatar */}
+            <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
+                {/* Search Box */}
+                <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
+                    <MagnifyingGlassIcon className="h-6 w-6 text-gray-500" />
+                    <input type="text" placeholder='Search' className='flex-1 outline-none p-2' />
+                    <button type="submit" hidden>Search</button>
+                </form>
+
+                {/* Avatar */}
+                <Avatar name="shinnkura" size="40" round className='ml-2' />
+            </div>
         </div>
     </header>
   )
