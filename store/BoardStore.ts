451b31e5326643263cdfd209235d5413f3e85687
shinnkura <n.shinzo1010@icloud.com>
2023-09-17 02:10:47 +0900

    Add : タスクボード全体の、大まかなUIの完成

---
+++ store/BoardStore.ts
@@ -0,0 +1,18 @@
+import { getTodoGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
+import { Board, Column, TypedColumn } from '@/typings';
+import { create } from 'zustand'
+
+interface BoardState {
+    board: Board;
+    getBoard: () => void;
+}
+
+export const useBoardStore = create<BoardState>((set) => ({
+  board: {
+    columns: new Map<TypedColumn, Column>()
+  },
+  getBoard: async() => {
+    const board = await getTodoGroupedByColumn();
+    set({ board });
+  }
+}))
---
+++ typings.d.ts
@@ -0,0 +1,26 @@
+import { type } from "os";
+import internal from "stream";
+
+interface Board {
+    columns: Map<TypedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "inprogress" | "done";
+
+interface Column {
+    id: TypedColumn;
+    todos: Todo[];
+}
+
+interface Todo {
+    $id: string;
+    $createdAt: string;
+    title: string;
+    status: TypedColumn;
+    image?: Image;
+}
+
+interface Image {
+    bucketId: string;
+    fileId: string;
+}
