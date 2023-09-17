008b5889810cfe64cdff0e61476092974fdd8a09
shinnkura <n.shinzo1010@icloud.com>
2023-09-17 09:49:49 +0900

    Add : タスクをドラッグアンドドロップできる機能を追加

--- components/Board.tsx
+++ components/Board.tsx
@@ -4,11 +4,13 @@ import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
 import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
+import { start } from 'repl';
 
 const Board = () => {
-    const [board, getBoard] = useBoardStore((state) => [
+    const [board, getBoard, setBoardState] = useBoardStore((state) => [
       state.board,
       state.getBoard,
+      state.setBoardState,
     ]);
 
     useEffect(() => {
@@ -16,8 +18,75 @@ const Board = () => {
     }, [getBoard]);
 
     const handleOnDragEnd = (result: DropResult) => {
-    // console.log(result);
-  }
+      const { destination, source, type } = result;
+
+      // Check if user dragged card outside of the board
+      if (!destination) return;
+
+      // Check if user dragged card to the same position
+      if (type === "column"){
+        const entries = Array.from(board.columns.entries());
+        const [removed] = entries.splice(source.index, 1);
+        entries.splice(destination.index, 0, removed);
+        const rearrangedColumns = new Map(entries);
+        setBoardState({
+          ...board,
+          columns: rearrangedColumns,
+        })
+      }
+
+      const columns = Array.from(board.columns);
+      const startColIndex = columns[Number(source.droppableId)];
+      const finishColIndex = columns[Number(destination.droppableId)];
+
+      const startCol = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+      };
+
+      const finishCol = {
+        id: finishColIndex[0],
+        todos: finishColIndex[1].todos,
+      };
+
+      if (!startCol || !finishCol) return;
+
+      if (source.index === destination.index && startCol === finishCol) return;
+
+      const newTodos = startCol.todos;
+      const [todoMoved] = newTodos.splice(source.index, 1);
+
+      if (startCol.id === finishCol.id) {
+        // same column task drag
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+        const newColumns = new Map(board.columns);
+        newColumns.set(newCol.id, newCol);
+
+        setBoardState({ ...board, columns: newColumns });
+      } else {
+        // different column task drag
+        const finishTodos = Array.from(finishCol.todos);
+        finishTodos.splice(destination.index, 0, todoMoved);
+
+        const newColumns = new Map(board.columns);
+        const newCol = {
+          id: finishCol.id,
+          todos: finishTodos,
+        };
+
+        newColumns.set(newCol.id, newCol);
+        newColumns.set(finishCol.id, {
+          id: finishCol.id,
+          todos: finishTodos,
+        });
+
+        setBoardState({ ...board, columns: newColumns });
+      }
+    };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -5,14 +5,19 @@ import { create } from 'zustand'
 interface BoardState {
     board: Board;
     getBoard: () => void;
+    setBoardState: (board: Board) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
   board: {
     columns: new Map<TypedColumn, Column>()
   },
+
   getBoard: async() => {
     const board = await getTodoGroupedByColumn();
     set({ board });
-  }
+  },
+
+  setBoardState: (board) => set({ board }),
+
 }))
